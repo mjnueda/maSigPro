616893daa895bf8bb28f50a71393aeb743881d69
Ana Conesa <aconesa@ivia.es>
2013-09-18 10:45:52 +0000

    maSigProUsersGuide.Rd Warning and First.lib deleted
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/maSigPro@80539 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-.First.lib <- function (libname, pkgname) 
-{
-
-if(.Platform$OS.type == "windows" && require(Biobase) && interactive() && .Platform$GUI == "Rgui") {
-addVigs2WinMenu("maSigPro")
-     }
-}
