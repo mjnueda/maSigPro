9af4e225facab9e6ec66cbcc9032796fd4325fab
Dan Tenenbaum <dtenenba@fhcrc.org>
2013-10-23 20:57:04 +0000

    put users guide in inst/doc since it doesn't get built
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/maSigPro@82069 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/maSigProUsersGuide.R
+++ R/maSigProUsersGuide.R
@@ -3,7 +3,7 @@ maSigProUsersGuide<-function (view = TRUE)
     f <- system.file("doc", "maSigProUsersGuide.pdf", package = "maSigPro")
     if (view) {
         if (.Platform$OS.type == "windows") 
-            shell.exec(f)
+            shell.exec(f) # consider using browseUrl() instead?
         else system(paste(Sys.getenv("R_PDFVIEWER"), f, "&"))
     }
     return(f)
