2062b7b9f6a4b32c55a71948a0c004d0f8984f72
Ana Conesa <aconesa@ivia.es>
2016-07-13 17:33:13 +0000

    Conditionals in stepback to avoid error when design is 1 group and 1 variable
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/maSigPro@119346 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/stepback.R
+++ R/stepback.R
@@ -3,6 +3,12 @@ stepback <- function (y = y, d = d, alfa = 0.05, family = gaussian() , epsilon=0
     lm1 <- glm(y ~ ., data = d, family=family, epsilon=epsilon)
     result <- summary(lm1)
     max <- max(result$coefficients[, 4][-1], na.rm = TRUE)
+    if (length(result$coefficients[, 4][-1]) == 1) {
+      if (max > alfa) {
+        max = 0 
+        lm1 <- glm(y ~ 1,  family=family, epsilon=epsilon)
+      }
+    }
     while (max > alfa) {
         varout <- names(result$coefficients[, 4])[result$coefficients[, 
             4] == max][1]
