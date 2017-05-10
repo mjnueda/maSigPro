8297c6a34a6f815fd30688243d12a7dd32f55755
Ana Conesa <aconesa@ivia.es>
2017-05-10 11:15:30 +0000

    New functions for IsomaSigPro
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/maSigPro@129507 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/getDSPatterns.R
@@ -0,0 +1,4 @@
+getDSPatterns <- function(tableDS, Cluster.Major, Cluster.minor)
+{
+ rownames(tableDS$IsoClusters)[tableDS$IsoClusters[,1]==Cluster.Major & tableDS$IsoClusters[,2]==Cluster.minor]
+}
